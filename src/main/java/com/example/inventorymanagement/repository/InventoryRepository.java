b2100c7260c6f24a21b1a366aa1b494aa34fad40
Aizirek Iskenderova <99177572+AiziliIsu@users.noreply.github.com>
2024-12-18 00:54:09 +0600

    Update InventoryRepository.java
    Adding new functionalities

--- src/main/java/com/example/inventorymanagement/repository/InventoryRepository.java
+++ src/main/java/com/example/inventorymanagement/repository/InventoryRepository.java
@@ -2,6 +2,16 @@ package com.example.inventorymanagement.repository;
 
 import com.example.inventorymanagement.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
 
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProduct_Id(Long productId);
+
+    List<Inventory> findByStockLessThan(int threshold); // For low-stock report
 }
+
+
+
