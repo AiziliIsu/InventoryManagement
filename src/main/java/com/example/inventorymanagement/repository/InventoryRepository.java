f2612779f49c8e6036ae9119aa79cd194bf4d300
Aizirek Iskenderova <aizirek.iskenderova_2026@ucentralasia.org>
2024-12-23 17:27:52 +0600

    Fixed conflicts and committed final changes

--- src/main/java/com/example/inventorymanagement/repository/InventoryRepository.java
+++ src/main/java/com/example/inventorymanagement/repository/InventoryRepository.java
@@ -2,6 +2,13 @@ package com.example.inventorymanagement.repository;
 
 import com.example.inventorymanagement.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
+
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProduct_Id(Long productId);
+    List<Inventory> findByStockLessThan(int threshold);
 }
--- src/main/java/com/example/inventorymanagement/repository/ProductRepository.java
+++ src/main/java/com/example/inventorymanagement/repository/ProductRepository.java
@@ -2,6 +2,11 @@ package com.example.inventorymanagement.repository;
 
 import com.example.inventorymanagement.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByCategory_Name(String categoryName);
 }
